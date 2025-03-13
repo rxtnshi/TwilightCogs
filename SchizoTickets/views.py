e275e369f4fdafd60533fca881124f998515eec5
Insurgent <rli60609@gmail.com>
2025-03-12 20:26:44 -0500

    Update views.py

--- SchizoTickets/views.py
+++ SchizoTickets/views.py
@@ -26,8 +26,6 @@ class TicketDropdown(discord.ui.Select):
 class BugReportModal(discord.ui.Modal):
 	def __init__(self):
 		super().__init__(title="Bug Report")
-		self.bot = bot
-
 		self.bug_description = discord.ui.TextInput(label="Describe the bug in a few sentences", required=True, style=discord.TextStyle.short)
 		self.bug_reproduce = discord.ui.TextInput(label="Reproduction Steps", required=True, style=discord.TextStyle.paragraph)
 		self.add_item(self.bug_description)
@@ -53,7 +51,7 @@ class BugReportModal(discord.ui.Modal):
 			value = self.bug_reproduce.value
 		)
 
-		await bug_report_channel.send(embed=embed, view=BugReportStatusView(self.bot))
+		await bug_report_channel.send(embed=embed)
 
 		# BUG REPORT CHANNEL CHECK
 		if not bug_report_channel:
