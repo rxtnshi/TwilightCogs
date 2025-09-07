1baf1db36f1b9c8e4bd5399733623560ef91fbfb
Insurgent <rli60609@gmail.com>
2025-09-07 17:46:39 -0500

    fix: add missing import statement for commands from redbot.core in TwilightTickets.py

--- TwilightTickets/TwilightTickets.py
+++ TwilightTickets/TwilightTickets.py
@@ -4,6 +4,7 @@ from .TwilightTickets import Tickets
 from datetime import datetime
 from discord import app_commands
 from discord.ext import commands
+from redbot.core import commands
 
 
 class TwilightTickets(commands.Cog):
