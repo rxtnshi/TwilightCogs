7c0ebb53fcc47de88b7a942cfe5d397d9913ac97
Insurgent <rli60609@gmail.com>
2025-09-08 23:30:51 -0500

    thing update

--- TwilightTickets/Tickets.py
+++ TwilightTickets/Tickets.py
@@ -194,8 +194,8 @@ async def create_ban_appeal(interaction, banned_user: str, appeal_request: str, 
         return
     
     embed = discord.Embed(title="📥 Ban Appeal Received", description=f"Submitted by {user.mention}", color=0xffa500)
-    embed.add_field(name="Banned Account ID", value=banned_user, inline=False)
-    embed.add_field(name="User's Appeal", value=appeal_request, inline=False)
+    embed.add_field(name="Platform and AccountID", value=banned_user, inline=False)
+    embed.add_field(name="Appeal Description", value=appeal_request, inline=False)
     embed.set_footer(text=f"User ID: {user.id} | Appeal ID: {appeal_id}")
 
     await appeals_channel.send(embed=embed, view=ViewsModals.AppealView())
