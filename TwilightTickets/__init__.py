e3758ac246e3bd1461bc5aec590b17e2114d149a
Insurgent <rli60609@gmail.com>
2025-09-09 00:25:28 -0500

    f

--- TwilightTickets/__init__.py
+++ TwilightTickets/__init__.py
@@ -8,3 +8,4 @@ async def setup_hook(bot):
     await bot.add_view(ViewsModals.CloseTicketView())
     await bot.add_view(ViewsModals.TicketView())
     await bot.add_view(ViewsModals.AppealView())
+    print("views added successfully")
