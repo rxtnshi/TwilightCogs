9e0a04f9e4df1c3fd078850feb983968b384f6d9
Insurgent <rli60609@gmail.com>
2025-09-10 00:24:22 -0500

    remove ephermal

--- TwilightTickets/ViewsModals.py
+++ TwilightTickets/ViewsModals.py
@@ -288,4 +288,4 @@ class FinishAppealModal(discord.ui.Modal):
         else:
             await original_message.edit(embed=new_embed)
 
-        await interaction.response.edit_message(f"✅ <@{opener_id}> has been successfully notified. Appeal status has been updated.", ephemeral=True)
+        await interaction.response.edit_message(f"✅ <@{opener_id}> has been successfully notified. Appeal status has been updated.")
