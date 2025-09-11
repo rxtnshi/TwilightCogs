59b2d92ec56d028e02b2067e7b008591aab0cae7
Insurgent <rli60609@gmail.com>
2025-09-10 19:36:57 -0500

    fix

--- TwilightTickets/__init__.py
+++ TwilightTickets/__init__.py
@@ -10,7 +10,7 @@ async def setup_hook(bot):
     guild = bot.get_guild(1341956884059521025)
 
     cog.tickets_enabled = await cog.config.guild(guild).tickets_enabled()
-    cog.tickets_enabled = await cog.config.guild(guild).ticket_statuses()
+    cog.tickets_statuses = await cog.config.guild(guild).ticket_statuses()
 
     bot.add_view(ViewsModals.TicketView())
     bot.add_view(ViewsModals.CloseTicketView())
