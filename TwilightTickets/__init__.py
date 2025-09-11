7130fb39a59fda3becc28d9458d7b00300bb0381
Insurgent <rli60609@gmail.com>
2025-09-10 19:34:47 -0500

    remove cog_load, move everything to setup_hook

--- TwilightTickets/__init__.py
+++ TwilightTickets/__init__.py
@@ -5,8 +5,15 @@ async def setup(bot):
     await bot.add_cog(TwilightTickets(bot))
 
 async def setup_hook(bot):
+    await bot.wait_until_ready()
+    cog = bot.get_cog("TwilightTickets")
+    guild = bot.get_guild(1341956884059521025)
+
+    cog.tickets_enabled = await cog.config.guild(guild).tickets_enabled()
+    cog.tickets_enabled = await cog.config.guild(guild).ticket_statuses()
+
     bot.add_view(ViewsModals.TicketView())
     bot.add_view(ViewsModals.CloseTicketView())
     bot.add_view(ViewsModals.AppealView())
-    
+
     
