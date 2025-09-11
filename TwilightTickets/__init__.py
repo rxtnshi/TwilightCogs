c75966fb3d1a743666ca6e0734a25e2ad8941328
Insurgent <rli60609@gmail.com>
2025-09-10 19:08:07 -0500

    revert changes

--- TwilightTickets/__init__.py
+++ TwilightTickets/__init__.py
@@ -5,6 +5,7 @@ async def setup(bot):
     await bot.add_cog(TwilightTickets(bot))
 
 async def setup_hook(bot):
+    await bot.wait_until_ready()
     bot.add_view(ViewsModals.TicketView())
     bot.add_view(ViewsModals.CloseTicketView())
     bot.add_view(ViewsModals.AppealView())
