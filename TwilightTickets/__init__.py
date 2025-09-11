37a6fe30057865fdf5f640c3c0f997db6ff42ba1
Insurgent <rli60609@gmail.com>
2025-09-10 20:17:26 -0500

    e

--- TwilightTickets/__init__.py
+++ TwilightTickets/__init__.py
@@ -4,12 +4,15 @@ from . import ViewsModals
 async def setup(bot):
     await bot.add_cog(TwilightTickets(bot))
 
+    
+
 async def setup_hook(bot):
     cog = bot.get_cog("TwilightTickets")
+    guild = bot.get_guild(1341956884059521025)
 
     bot.add_view(ViewsModals.TicketView())
     bot.add_view(ViewsModals.CloseTicketView())
     bot.add_view(ViewsModals.AppealView())
     
-    cog.ticket_statuses = await cog.config.get_raw("tickets_enabled")
-    cog.tickets_enabled = await cog.config.get_raw("ticket_statuses")
+    cog.ticket_statuses = await cog.config.guild(guild).get_raw("ticket_statuses")
+    cog.tickets_enabled = await cog.config.guild(guild).get_raw("tickets_enabled")
